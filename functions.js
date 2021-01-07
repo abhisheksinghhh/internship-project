cd4bdc2e96fe2329ebd9803b53dc1da6c316306f
abhisheksinghhh <abhisheksingh3117@gmail.com>
2021-01-08 00:19:04 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,23 @@
+function fetchinformation(done) {
+  $.get("/api/information", function (data) {
+    done(data);
+  });
+}
+
+function addinformation(name, email, phone, city, service) {
+  $.post(
+    "/api/information",
+    {
+      name: name,
+      email: email,
+      phone: phone,
+      city: city,
+      service: service,
+    },
+    function (done) {
+      done(data);
+    }
+  );
+}
+
+function addcard()
